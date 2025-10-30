26570c552471784e2875fd1475e270d30d3d3f8e
brian <contactbrianlopez@gmail.com>
2025-10-29 22:33:06 -0700

    ridgeform_starter

--- app/root.tsx
+++ app/root.tsx
@@ -11,16 +11,12 @@ import type { Route } from "./+types/root";
 import "./app.css";
 
 export const links: Route.LinksFunction = () => [
-  { rel: "preconnect", href: "https://fonts.googleapis.com" },
-  {
-    rel: "preconnect",
-    href: "https://fonts.gstatic.com",
-    crossOrigin: "anonymous",
-  },
-  {
-    rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
-  },
+    { rel: "icon", type: "image/png", href: "/favicon/favicon-96x96.png", sizes: "96x96" },
+    { rel: "icon", type: "image/svg+xml", href: "/favicon/favicon.svg" },
+    { rel: "shortcut icon", href: "/favicon/favicon.ico" },
+    { rel: "apple-touch-icon", sizes: "180x180", href: "/favicon/apple-touch-icon.png" },
+    { rel: "manifest", href: "/favicon/site.webmanifest" },
+
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,49 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Ridgeform Construction | Building innovation, quality, and community in Seattle." },
+    { name: "description", content: "Ridgeform Construction is a Seattle construction firm focused on innovation, quality, and community development. Our mission is to build spaces that bring people together and contribute to the region’s long-term growth and livability." },
+    { tagName: "meta", name: "apple-mobile-web-app-title", content: "Ridgeform" },
+    { name: "language", content: "en"},
+       // Open Graph
+    { property: "og:type", content: "website" },
+    {
+      property: "og:title",
+      content: "Ridgeform Construction | Building innovation, quality, and community in Seattle.",
+    },
+    {
+      property: "og:description",
+      content:
+        "Ridgeform Construction is a Seattle construction firm focused on innovation, quality, and community development. Our mission is to build spaces that bring people together and contribute to the region’s long-term growth and livability.",
+    },
+    { property: "og:url", content: "https://ridgeformconstruction.com/" },
+    { property: "og:image", content: "https://www.ridgeformconstruction.com.com/assets/ridgeform_logo.png" },
+
+    // Twitter
+    { name: "twitter:card", content: "summary_large_image" },
+    {
+      name: "twitter:title",
+      content: "Ridgeform Construction | Building innovation, quality, and community in Seattle.",
+    },
+    {
+      name: "twitter:description",
+      content:
+        "Ridgeform Construction is a Seattle construction firm focused on innovation, quality, and community development. Our mission is to build spaces that bring people together and contribute to the region’s long-term growth and livability.",
+    },
+    {
+      name: "twitter:image",
+      content: "https://www.ridgeformconstruction.com.com/assets/ridgeform_logo.png",
+    },
+
   ];
 }
 
+
 export default function Home() {
-  return <Welcome />;
+  return (
+    <>
+    Website is under construction
+    </>
+  );
 }
